30759136996b198f75a32d7fc0c19075a1e0e0d6
farid <faridnubaili@gmail.com>
2025-01-11 22:16:00 +0700

    try fix deployed page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 import { Education } from './components/Education';
 
-const App = () => {
+function App(){
   const [activeSection, setActiveSection] = useState('hero');
 
   useEffect(() => {
