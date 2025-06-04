72446e80f415f3f0625dcf40860d5a954a221344
farid nubaili <farid.nubaili@tritronik.com>
2025-06-05 05:24:59 +0700

    update content

--- src/components/common/YoutubeEmbed.jsx
+++ src/components/common/YoutubeEmbed.jsx
@@ -1,10 +1,15 @@
 export const YouTubeEmbed = ({ videoId }) => (
-  <div className="relative w-full pt-[56.25%]">
+  <>
+  {videoId&&<div className="relative w-full pt-[56.25%]">
     <iframe
       src={`https://www.youtube.com/embed/${videoId}`}
       className="absolute top-0 left-0 w-full h-full rounded-t-lg"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
     />
-  </div>
+  </div>}
+  {!videoId&&<div className="relative w-full pt-[56.25%]">
+    No Video
+  </div>}
+  </>
 );
