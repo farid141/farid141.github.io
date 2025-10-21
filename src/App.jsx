9ec8f00b60b0c828f7d5377d2b0d6dbc0d0ba2c7
farid nubaili <farid.nubaili@tritronik.com>
2025-10-21 16:20:50 +0700

    add download cv button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Navbar } from "./Layout/Navbar";
 import { Hero } from "./components/Hero";
 import { Experience } from "./components/Experience";
@@ -6,6 +5,7 @@ import { Skills } from "./components/Skills";
 import { Education } from "./components/Education";
 import ProjectTabs from "./components/ProjectTabs";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Download } from "lucide-react";
 
 function App() {
   const activeSection = window.location.hash;
@@ -18,6 +18,14 @@ function App() {
         <ProjectTabs />
         <Skills />
         <Education />
+        <a 
+          href="https://drive.google.com/file/d/1CvIfpaLNZ_xG3ohwHTWmUm-taIodDAgV/view?usp=sharing" 
+          target="_blank"
+          className="fixed bottom-6 right-6 z-50 shadow-lg flex gap-2 bg-primary text-primary-foreground p-5 rounded-full"
+        >
+          <Download className="h-5 w-5" />
+          Download CV
+        </a>
       </div>
     </ThemeProvider>
   );
